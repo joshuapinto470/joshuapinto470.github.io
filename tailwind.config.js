ae3c35d1e95cf5605670439939244f5ca3cc7a46
Joshua Pinto <joshuapinto470@gmail.com>
2023-07-04 11:28:41 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        'Poppins' : ['Poppins', 'sans-serif'],
+      },
+      colors : {
+        'primary-text' : '#33333d',
+      }
+    },
+  },
+  plugins: [],
+}
